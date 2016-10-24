b9c2768844df3b33490158e8aba33089d1b3136e
vanderci c junior <vandercijr@gmail.com>
2016-10-24 21:17:25 -0200

    Implementing quickSort

--- lib/sort.c
+++ lib/sort.c
@@ -1,4 +1,18 @@
-void quicksort(* char vector, int left, int right)
+void quicksort(char * arr, int left, int right)
 {
+    int l,r pivot;
+
+    l     = left;
+    r     = right;
+    pivot = arr[(int)(l+r)/2];
+
+    while (l < r) {
+      while (l < arr[pivot]) {
+        l++;
+      }
+      while (r > arr[pivot]) {
+        r--;
+      }
+    }
 
 }
